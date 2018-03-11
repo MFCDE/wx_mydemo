86e2187ab9c33f6fa8f2ee599098a226430f1dbf
wk <76235150@qq.com>
2018-03-11 21:48:33 +0800

    '第一次编辑'

--- data/post-data.js
+++ data/post-data.js
@@ -21,7 +21,7 @@ var local_database = [{
         img_condition: true,
         content: "秋天是吃螃蟹的季节，“金秋菊黄蟹正肥”中秋的螃蟹是最好吃的。农历八月，天气渐凉，桂花飘香，金菊竞放，游荡在河汊湖荡里的螃蟹也到了一年中最为肥美的时节。“看你横行到几时?”看看这诗句就知道,真的到了吃螃蟹的时候了。",
         reading: "112",
-        collection: "96",
+        collection: "100",
         fal: false
     }
 ];
